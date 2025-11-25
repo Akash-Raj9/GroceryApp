1457c376676a1278757ba7933d3db0af5348493d
Akash-Raj9 <gopakash74@gmail.com>
2025-11-25 16:53:08 +0530

    backend_initial-commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+
+const allowedOrigins = ["http://localhost:5173"];
+
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin:allowedOrigins,
+    Credentials:true
+}))
+
+
+app.get('/',(req,res)=>{
+    res.send("Api is working");
+})
+
+
+app.listen(port,()=>{
+    console.log(`server is running on port: ${port}`);
+})
+
